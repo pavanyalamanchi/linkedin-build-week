d0dc1a03c350235b49d9cff46c278ba3697c2d7f
Luna Sarmiento <selene942008@hotmail.com>
2021-07-19 16:17:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <div></div>
     </div>
   );
 }
