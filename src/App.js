8ade4b27d7b3078f53578e38fe30eb4bf641b648
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-19 16:29:46 +0200

    Minor Tweaks to core elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Sidebar  from './components/Sidebar'
 
 function App() {
   return (
     <>
       <div className="App">
+        <Sidebar />
       </div>
     </>
   );
