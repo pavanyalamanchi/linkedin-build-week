b23e9a65953b5cf76a4e34c0e90423362e80db44
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-19 11:16:10 +0200

    Merged Development into marcelo/home-sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+	render() {
+		return (
+			<>
+        <div className="sidebar-main-container">
+  				
+  			</div>
+      </>
+		)
+	}
+}
