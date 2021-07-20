01aeb12a6258d1fcdb3bbca85034b35141df99ae
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-20 22:51:57 +0200

    Create Experience.jsx

---
+++ src/components/Experience.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Experience(props) {
+	return (
+		<a href="" className="my-3 experience-fragment-container">
+			<div className="experience-fragment-company-image">
+        <img src="" alt="" />
+      </div>
+      <div className="px-3 experience-fragment-details">
+        <h6 className="fw-400 experience-fragment-title">
+          Quality Assurance
+        </h6>
+        <p className="experience-fragment-text">
+          Undercurrent Audio • Freelance
+        </p>
+        <p className="experience-fragment-text text-secondary">
+          Jan 2020 - Present • 1 year 7 mos.
+        </p>
+        <p className="experience-fragment-text text-secondary">
+          Berlin, Germany
+        </p>
+        <p className="experience-fragment-description my-2 text-decoration-none">
+          I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. You have so many different things placeholder text has to be able to do, and I don’t believe Lorem Ipsum has the stamina. The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive. I have a 10 year old son. He has words. He is so good with these words it’s unbelievable.
+        </p>
+      </div>
+		</a>
+  )
+}
+
+
