707ee45d737e3b8b02b9933e0bcee00fcc6195c1
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-20 22:51:51 +0200

    Create EditDataButton.jsx

---
+++ src/components/EditDataButton.jsx
@@ -0,0 +1,29 @@
+import { Modal, Button } from 'react-bootstrap'
+
+export default function EditDataButton(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Update experience
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer className="position-absolute bottom-0">
+        <Button onClick={props.onHide}>Save</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
