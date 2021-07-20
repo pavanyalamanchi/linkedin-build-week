805ff80f97214beeac9a57317ac57e844455a014
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-20 23:54:58 +0200

    added dynamic props to Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,30 +1,40 @@
 import React from 'react'
+import faker from 'faker'
 
 export default function Experience(props) {
-	return (
+	console.log(props)
+  return (
 		<a href="" className="my-3 experience-fragment-container">
 			<div className="experience-fragment-company-image">
-        <img src="" alt="" />
+        <img className="experience-fragment-image" src={faker.image.business()} alt="" />
       </div>
       <div className="px-3 experience-fragment-details">
         <h6 className="fw-400 experience-fragment-title">
-          Quality Assurance
+          {props.experienceData.role}
         </h6>
         <p className="experience-fragment-text">
-          Undercurrent Audio • Freelance
+          {props.experienceData.company}
         </p>
         <p className="experience-fragment-text text-secondary">
-          Jan 2020 - Present • 1 year 7 mos.
+          {convertDateToString(props.experienceData.startDate, props.experienceData.endDate)} 
         </p>
         <p className="experience-fragment-text text-secondary">
-          Berlin, Germany
+          {props.experienceData.area}
         </p>
         <p className="experience-fragment-description my-2 text-decoration-none">
-          I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. You have so many different things placeholder text has to be able to do, and I don’t believe Lorem Ipsum has the stamina. The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive. I have a 10 year old son. He has words. He is so good with these words it’s unbelievable.
+          {props.experienceData.description}
         </p>
       </div>
 		</a>
   )
 }
 
-
+function convertDateToString (start, end) {
+  console.log(start)
+  console.log(end)
+  if (end === null) {
+    return (start + " - Present")
+  } else {
+    return (start + " - " + end)
+  }
+}
