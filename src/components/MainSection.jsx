2245fa441071f4d3dfac0fa111834afaae6a8655
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-20 11:19:09 +0200

    Reformat of All MainSection Code

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -10,127 +10,160 @@ const MainSection = () => {
       <Row className="justify-content-center">
         <Col sm={8} className="user-info mt-2">
           <Card>
-           <div className="cover-image"> <Card.Img className="cover-img" variant="top" src={cover} /></div>
+            <div className="cover-image"> 
+              <Card.Img className="cover-img" variant="top" src={cover} />
+            </div>
             <img src={profile} alt="#" className="profile-pic rounded-circle" />
+            
             <Card.Body className="user-info-body">
               <Row className="justify-content-space-between rows-col-md-6 rows-col-sm-12 rows-col-sx-12 ">
+                
                 <Col>
-            
-                    <div className="d-flex mt-3">
-                      <h2 className="member-name">Elon Musk <span>. 1st</span></h2>
-                    </div>
-                   <div><h6 className="member-position">CEO Tesla SpaceX</h6></div>
-                    <div className="d-flex">
-                      <p className="text-muted"> Berlin Metropolitan Area . <a
-                        href=".sample"
-                        alt="#"
-                        style={{ textDecoration: "none", fontWeight: "600", color:"#004182" }}
-                      >
+                  <div className="d-flex mt-3">
+                    <h2 className="member-name">Elon Musk <span>. 1st</span></h2>
+                  </div>
+                  <div>
+                    <h6 className="member-position">CEO Tesla SpaceX</h6>
+                  </div>
+                  <div className="d-flex">
+                    <p className="text-muted"> 
+                      Berlin Metropolitan Area . 
+                      <a href=".sample" alt="#" className="profile-contact-info" style={{ textDecoration: "none", fontWeight: "600", color:"#004182" }} >
                         Contact Info
-                      </a></p>
-                    </div>
-                 
+                      </a>
+                    </p>
+                  </div>
                 </Col>
+                
                 <Col className="col d-flex justify-content-center">
                   <ListGroup variant='flush' className='mt-3' >
-                    <ListGroup.Item style={{borderBottom:'1px solid white'}}><SiTesla style={{marginRight:'0.8rem'}}/>Tesla</ListGroup.Item>
-                    <ListGroup.Item><SiSpacex style={{marginRight:'0.8rem'}}/>SpaceX</ListGroup.Item>
+                    <ListGroup.Item style={{borderBottom:'1px solid white'}}>
+                      <SiTesla style={{marginRight:'0.8rem'}} />
+                      Tesla
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <SiSpacex style={{marginRight:'0.8rem'}} />
+                        SpaceX
+                    </ListGroup.Item>
                   </ListGroup>
                 </Col>
               </Row>
               
-                 <div> <a href='./sample' style={{ textDecoration: "none", fontWeight: "600", fontSize:'medium' }}>500+ Connections</a></div>
-                  <div className='' style={{marginTop:'0.8rem'}}>
-        <Button className='btn btn-info text-light rounded-pill mb-2' style={{backgroundColor:'#0a66c2', borderColor:'#0a66c2'}}> Open To</Button>
-                    <button className='btn btn-outline-secondary rounded-pill mb-2'>Add section</button>
-                    <button className='btn btn-outline-secondary rounded-pill mb-2'>More</button>
-                  </div>
-             
+              <div>
+                <a href='./sample' style={{ textDecoration: "none", fontWeight: "600", fontSize:'medium' }}>
+                  500+ Connections
+                </a>
+              </div>
+              <div className='' style={{marginTop:'0.8rem'}}>
+                <Button className='btn btn-info text-light rounded-pill mb-2' style={{backgroundColor:'#0a66c2', borderColor:'#0a66c2'}}> 
+                  Open To
+                </Button>
+                <button className='btn btn-outline-secondary rounded-pill mb-2'>Add section</button>
+                <button className='btn btn-outline-secondary rounded-pill mb-2'>More</button>
+              </div>
             </Card.Body>
           </Card>
 
           <Card>
-          <Card.Body>
-            <h3>About</h3>
-            <p>Experienced Senior Web Application Developer with a demonstrated history of working in the computer software industry. Skilled in Negotiation,Web Development,Mobile App development, C++, Content Management Systems (CMS), Management, and HTML. Strong engineering professional with a Master’s of Computer Science focused in Computer Science from Halmstad University, Sweden.</p>
-          </Card.Body>
+            <Card.Body>
+              <h3>About</h3>
+              <p>Experienced Senior Web Application Developer with a demonstrated history of working in the computer software industry. Skilled in Negotiation,Web Development,Mobile App development, C++, Content Management Systems (CMS), Management, and HTML. Strong engineering professional with a Master’s of Computer Science focused in Computer Science from Halmstad University, Sweden.</p>
+            </Card.Body>
           </Card>
 
           <Card>
-          <Card.Body>
-            <h3>Activity</h3>
-            <div><span style={{color: "#0a66c2", fontWeight:"500"}}>1,844 followers</span></div>
-            <Row>
-              <Col>
-                <div className="card mb-3">
-                  <div className="row no-gutters">
-                    <div className="col-md-2">
-                    <Card.Img className="cover-img" variant="top" src={cover} style={{height:"60px", width: "60px"}} />
-                    </div>
-                    <div className="col-md-10">
-                      <div className="card-body">
-                        <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>Simulation gear reducer</h5>
-                        <p className="card-text">Elon Musk shared this  </p>
-                        <p className="card-text"><small class="text-muted">3 React</small><small class="text-muted">1 Comment</small></p>
+            <Card.Body>
+              <h3>Activity</h3>
+              <div><span style={{color: "#0a66c2", fontWeight:"500"}}>1,844 followers</span></div>
+              <Row>
+                <Col>
+                  <div className="card mb-3">
+                    <div className="row no-gutters">
+                      <div className="col-md-2">
+                        <Card.Img className="cover-img" variant="top" src={cover} style={{height:"60px", width: "60px"}} />
+                      </div>
+                      <div className="col-md-10">
+                        <div className="card-body">
+                          <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>Simulation gear reducer</h5>
+                          <p className="card-text">Elon Musk shared this  </p>
+                          <p className="card-text"><small class="text-muted">3 React</small><small class="text-muted">1 Comment</small></p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="card mb-3">
-                  <div className="row no-gutters">
-                    <div className="col-md-2">
-                    <Card.Img className="cover-img" variant="top" src={cover} style={{height:"60px", width: "60px"}} />
-                    </div>
-                    <div className="col-md-10">
-                      <div className="card-body">
-                        <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>Simulation gear reducer</h5>
-                        <p className="card-text">Elon Musk shared this  </p>
-                        <p className="card-text"><small class="text-muted">3 React</small><small class="text-muted">1 Comment</small></p>
+                  <div className="card mb-3">
+                    <div className="row no-gutters">
+                      <div className="col-md-2">
+                      <Card.Img className="cover-img" variant="top" src={cover} style={{height:"60px", width: "60px"}} />
+                      </div>
+                      <div className="col-md-10">
+                        <div className="card-body">
+                          <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>Simulation gear reducer</h5>
+                          <p className="card-text">Elon Musk shared this  </p>
+                          <p className="card-text"><small class="text-muted">3 React</small><small class="text-muted">1 Comment</small></p>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </Col>
-              <Col>
-              <div className="card mb-3">
-                  <div className="row no-gutters">
-                    <div className="col-md-2">
-                    <Card.Img className="cover-img" variant="top" src={cover} style={{height:"60px", width: "60px"}} />
-                    </div>
-                    <div className="col-md-10">
-                      <div className="card-body">
-                        <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>Simulation gear reducer</h5>
-                        <p className="card-text">Elon Musk shared this  </p>
-                        <p className="card-text"><small class="text-muted">3 React</small><small class="text-muted">1 Comment</small></p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              <div className="card mb-3">
-                  <div className="row no-gutters">
-                    <div className="col-md-2">
-                    <Card.Img className="cover-img" variant="top" src={cover} style={{height:"60px", width: "60px"}} />
-                    </div>
-                    <div className="col-md-10">
-                      <div className="card-body">
-                        <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>Simulation gear reducer</h5>
-                        <p className="card-text">Elon Musk shared this  </p>
-                        <p className="card-text"><small class="text-muted">3 React</small><small class="text-muted">1 Comment</small></p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
                 </Col>
-            </Row>
-            <Button href="#" variant="light" size="lg" style={{width: "100%"}}> See all activity</Button>
-          </Card.Body>
+                <Col>
+                  <div className="card mb-3">
+                    <div className="row no-gutters">
+                      <div className="col-md-2">
+                        <Card.Img className="cover-img" variant="top" src={cover} style={{height:"60px", width: "60px"}} />
+                      </div>
+                      <div className="col-md-10">
+                        <div className="card-body">
+                          <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>
+                            Simulation gear reducer
+                          </h5>
+                          <p className="card-text">
+                            Elon Musk shared this  
+                          </p>
+                          <p className="card-text">
+                            <small class="text-muted">3 React</small>
+                            <small class="text-muted">1 Comment</small>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card mb-3">
+                    <div className="row no-gutters">
+                      <div className="col-md-2">
+                        <Card.Img 
+                          className="cover-img" 
+                          variant="top" 
+                          src={cover} 
+                          style={{height:"60px", width: "60px"}} />
+                      </div>
+                      <div className="col-md-10">
+                        <div className="card-body">
+                          <h5 className="" style={{fontSize: "16px", lineHeight:"5px"}}>
+                            Simulation gear reducer
+                          </h5>
+                          <p className="card-text">Elon Musk shared this  </p>
+                          <p className="card-text">
+                            <small class="text-muted">3 React</small>
+                            <small class="text-muted">1 Comment</small>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+              <Button href="#" variant="light" size="lg" style={{width: "100%"}}> 
+                See all activity
+              </Button>
+            </Card.Body>
           </Card>
         </Col>
-        <Col sm={4}> <Sidebar />
+        <Col sm={4}> 
+          <Sidebar />
         </Col>
       </Row>
       <Row className="justify-content-center">
-        
       </Row>
     </Container>
   );
