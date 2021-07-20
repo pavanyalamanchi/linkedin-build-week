7d49d28d4c8a563b303bf1149b5417636df5230f
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-20 22:51:48 +0200

    Create AddDataButton.jsx

---
+++ src/components/AddDataButton.jsx
@@ -0,0 +1,43 @@
+import Modal from 'react-bootstrap/Modal'
+{/* <AddForm /> GOES IN BIG SPACE!!!!!! */}
+
+export default function AddExperience(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add experience
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+      </Modal.Body>
+    </Modal>
+  );
+}
