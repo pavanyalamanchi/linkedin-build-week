eb7549f559c2a9084badcdb3dcd11a798f38bb9a
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 12:48:24 +0200

    Added Route functionality to App. Preparing sign-in feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MainSection from './components/MainSection'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import MeSection from './components/MeSection'
 import Footer from './components/Footer'
-import TopNav from './components/TopNav';
+import TopNav from './components/TopNav'
+import ProfileSection from './components/ProfileSection'
+import FeedMain from './components/feed/FeedMain'
 
 function App() {
+  
+  const userSignIn = () => {
+    
+  }
+  
+  
   return (
     <>
-      <div className="App">
-        <TopNav />
-        <MainSection />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <TopNav />
+          <Route exact path="/" render={(routerProps) => <FeedMain /> } />
+          <Route exact path="/feed" render={(routerProps) => <FeedMain /> } />          
+          <Route exact path="/me/:id" render={(routerProps) => <MeSection /> } />
+          <Route exact path="/profile/:id" render={routerProps => <ProfileSection /> } />
+          <Footer />
+        </div>
+      </Router>
     </>
   );  
 }
