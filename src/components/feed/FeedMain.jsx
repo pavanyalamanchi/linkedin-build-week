98eaffef3d56c011ea7e729d90223435cb0e4e6b
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 13:20:24 +0200

    Router tweaks

--- src/components/feed/FeedMain.jsx
+++ src/components/feed/FeedMain.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import Sidebar from '../Sidebar'
+import { withRouter } from 'react-router-dom'
 
-export default class FeedMain extends Component {
-	render() {
-		return (
-			<div>
-          <Container>
-            <Row>
-              <Col sm={3}> <Sidebar /></Col>
-              <Col sm={5}></Col>
-              <Col sm={4}> <Sidebar /></Col>
-            </Row>
-          </Container>
-        </div>
-		)
-	}
+class FeedMain extends Component {
+  
+  
+  render() {
+    {console.log("this.props", this.props)}
+    return (
+    	<div>
+        <Container>
+          <Row>
+            <Col sm={3}> <Sidebar /></Col>
+            <Col sm={5}> </Col>
+            <Col sm={4}> <Sidebar /></Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
+
+export default withRouter(FeedMain)
