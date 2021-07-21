e9849b4e2bc4478797ab2b6733d7bfeffb5445e5
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-07-21 20:58:51 +0200

    user post

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MainSection from './components/MainSection'
 import Footer from './components/Footer'
 import TopNav from './components/TopNav';
+import NewPost from './components/NewPost';
+import CenteredModal from './components/PostModal';
 
 function App() {
   return (
     <>
       <div className="App">
         <TopNav />
-        <MainSection />
-        <Footer />
+        
+        <NewPost />
+        {/* <MainSection />
+        <Footer /> */}
       </div>
     </>
   );  
