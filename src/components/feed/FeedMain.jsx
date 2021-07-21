f35ff542d7b078f69cb3c741974c5ef8e02b071f
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-07-21 14:25:45 +0200

    create components

--- src/components/feed/FeedMain.jsx
+++ src/components/feed/FeedMain.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import FeedLeftSidebar from './FeedLeftSidebar'
 import Sidebar from '../Sidebar'
 import { withRouter } from 'react-router-dom'
+
 
 class FeedMain extends Component {
   
   
   render() {
-    {console.log("this.props", this.props)}
     return (
     	<div>
         <Container>
           <Row>
-            <Col sm={3}> <Sidebar /></Col>
+            <Col sm={3}> <FeedLeftSidebar /></Col>
             <Col sm={5}> </Col>
             <Col sm={4}> <Sidebar /></Col>
           </Row>
