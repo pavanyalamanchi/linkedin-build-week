f4a6a5d2a0a6fc654153cb153a857f7982d99782
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 10:04:29 +0200

    Update ExperienceSection Refactoring

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -5,12 +5,14 @@ import Loading from './Loading'
 
 export default function ExperienceSection (props) {
   const [experiences, setExperiences] = useState(null)
+  console.log("Should be null?", experiences)
   
   useEffect(() => {
     fetchExperiences()
   }, [])
   
   if (experiences === null){
+    console.log("Loading!")
     return <Loading />
   } else {
   	return (
@@ -19,17 +21,13 @@ export default function ExperienceSection (props) {
           <h5 className="profile-body-section-header my-2 mb-4">
             Experience
           </h5>
-          {
-            experiences.map(exp => (
+          { experiences.map(exp => (
               <Experience experienceData={exp} />  
-            ))
-          }
+          ))}
         </Card.Body>
   		</div>
   	)
   }
-  
-  
   
   async function fetchExperiences() {
     const userId = "60f53b250efe7800155c34a0"
@@ -42,25 +40,6 @@ export default function ExperienceSection (props) {
       })
       const data = await fetchExp.json()
       setExperiences( data )      
-    } catch (err) {
-      console.log(err)
-    }
-  }
-  
-  function fetchExperiencesTwo() {
-    const userId = "60f53b250efe7800155c34a0"
-    try {
-      fetch(`https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences`, {
-        headers: {
-          "Content-Type":"application/json",
-          "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGY1M2IyNTBlZmU3ODAwMTU1YzM0YTAiLCJpYXQiOjE2MjY2ODQxOTcsImV4cCI6MTYyNzg5Mzc5N30.3ZXfLM8Xio4MkKGlFiTA42FVjeiUinuO7VDCroKKFMw"
-        }
-      })
-        .then(response => response.json())
-        .then(data => {
-          setExperiences( data )
-          console.log("Experiences:", data)
-        })
     } catch (err) {
       console.log(err)
     }
