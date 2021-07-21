7636f89a9cbb94eab96fe5c975feca74c5852643
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 10:04:01 +0200

    Update Experience for dynamic edit button

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import faker from 'faker'
+import EditDataButton from './EditDataButton'
+import { Button, } from 'react-bootstrap'
 
 export default function Experience(props) {
 	console.log(props)
+  
+  const handleShow = () => {
+    console.log("Handle show!")
+  }
+  
   return (
-		<a href="" className="my-3 experience-fragment-container">
-			<div className="experience-fragment-company-image">
-        <img className="experience-fragment-image" src={faker.image.business()} alt="" />
-      </div>
-      <div className="px-3 experience-fragment-details">
-        <h6 className="fw-400 experience-fragment-title">
-          {props.experienceData.role}
-        </h6>
-        <p className="experience-fragment-text">
-          {props.experienceData.company}
-        </p>
-        <p className="experience-fragment-text text-secondary">
-          {convertDateToString(props.experienceData.startDate, props.experienceData.endDate)} 
-        </p>
-        <p className="experience-fragment-text text-secondary">
-          {props.experienceData.area}
-        </p>
-        <p className="experience-fragment-description my-2 text-decoration-none">
-          {props.experienceData.description}
-        </p>
-      </div>
-		</a>
+    <>
+  		<div href="" className="my-3 experience-fragment-container">
+  			<div className="experience-fragment-company-image">
+          <img className="experience-fragment-image" src={faker.image.business()} alt="" />
+        </div>
+        
+        <div className="px-3 experience-fragment-details">
+          
+          <div className="experience-fragment-header">
+            <h6 className="fw-400 experience-fragment-title">
+              {props.experienceData.role}
+            </h6>
+            <div className="experience-fragment-edit-container">
+              <EditDataButton />
+            </div>
+          </div>
+          
+          <p className="experience-fragment-text">
+            {props.experienceData.company}
+          </p>
+          <p className="experience-fragment-text text-secondary">
+            {convertDateToString(props.experienceData.startDate, props.experienceData.endDate)} 
+          </p>
+          <p className="experience-fragment-text text-secondary">
+            {props.experienceData.area}
+          </p>
+          <p className="experience-fragment-description my-2 text-decoration-none">
+            {props.experienceData.description}
+          </p>
+        </div>
+  		</div>
+      
+      
+    </>
   )
 }
 
