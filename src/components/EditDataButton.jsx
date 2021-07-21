0cfb95526e6c101f099ca6c16922e7f77dc4bba2
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 10:03:00 +0200

    Edit Data Button Component & Modal Logic

--- src/components/EditDataButton.jsx
+++ src/components/EditDataButton.jsx
@@ -1,29 +1,45 @@
 import { Modal, Button } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { VscEdit } from "react-icons/vsc";
 
 export default function EditDataButton(props) {
+  
+  let [show, setShow] = useState(false)
+  
+  const handleShow = () => { setShow(true) }
+  const handleClose = () => { setShow(false) }
+  
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Update experience
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer className="position-absolute bottom-0">
-        <Button onClick={props.onHide}>Save</Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <VscEdit
+        className="icons experience-fragment-edit-button"
+        onClick={handleShow}>
+       </VscEdit>
+      <Modal
+        {...props}
+        show={show}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        onHide={handleClose}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Update experience
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer className="position-absolute bottom-0">
+          <Button onClick={props.onHide}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
