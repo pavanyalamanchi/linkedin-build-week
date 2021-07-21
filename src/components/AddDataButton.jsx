49840f3cb881803d48ab47dcddaef7e4ea8bcc39
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 11:12:06 +0200

    Added Data Button functionality

--- src/components/AddDataButton.jsx
+++ src/components/AddDataButton.jsx
@@ -1,43 +1,42 @@
 import Modal from 'react-bootstrap/Modal'
-{/* <AddForm /> GOES IN BIG SPACE!!!!!! */}
+import AddForm from './AddForm'
+import { FiPlus } from "react-icons/fi";
+import {useState} from 'react'
+import { Button } from 'react-bootstrap'
 
 export default function AddExperience(props) {
+  let [show, setShow] = useState(false)
+  const handleShow = () => { setShow(true) }
+  const handleClose = () => { setShow(false) }
+  
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add experience
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-      </Modal.Body>
-    </Modal>
+    <>
+      <div className="experience-fragment-add-button-container">
+        <FiPlus
+          className="icons experience-fragment-add-button"
+          onClick={handleShow}>
+         </FiPlus>
+      </div>
+      <Modal
+        {...props}
+        show={show}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        onHide={handleClose}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Update experience
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <AddForm />
+        </Modal.Body>
+        <Modal.Footer className="position-absolute bottom-0">
+          <Button onClick={props.onHide}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
