0067b894279ec400eae0b8f5b2b890ff84266a6e
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 17:01:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Sidebar  from './components/Sidebar'
 import Footer from './components/Footer'
+import TopNav from './components/TopNav'
+import ProfileSection from './components/ProfileSection'
+import FeedMain from './components/feed/FeedMain'
+import SignInPage from './components/SignInPage'
 
 function App() {
   return (
     <>
-      <div className="App">
-        <Sidebar />
+      <Router>
+        <Route exact path="/signin" render={routerProps => <SignInPage />} />
+        <Route exact path="/" render={(routerProps => <Home />)}
         <Footer />
-      </div>
+      </Router>
     </>
   );
 }
