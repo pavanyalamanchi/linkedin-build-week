9b7870cb0025c01025fecf2a89229f673a4153e9
Luna Sarmiento <selene942008@hotmail.com>
2021-07-21 16:37:06 +0200

    Update FeedMain.jsx

--- src/components/feed/FeedMain.jsx
+++ src/components/feed/FeedMain.jsx
@@ -1,26 +1,30 @@
-import React, { Component } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import FeedLeftSidebar from './FeedLeftSidebar'
-import Sidebar from '../Sidebar'
-import { withRouter } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import FeedLeftSidebar from "./FeedLeftSidebar";
+import Sidebar from "../Sidebar";
+import { withRouter } from "react-router-dom";
+import PostsContainer from "../PostsContainer";
 
 class FeedMain extends Component {
-  
-  
   render() {
     return (
-    	<div>
+      <div>
         <Container>
           <Row>
-            <Col sm={3}> <FeedLeftSidebar /></Col>
-            <Col sm={5}> </Col>
-            <Col sm={4}> <Sidebar /></Col>
+            <Col sm={3}>
+              <FeedLeftSidebar />
+            </Col>
+            <Col sm={5}>
+              <PostsContainer />
+            </Col>
+            <Col sm={4}>
+              <Sidebar />
+            </Col>
           </Row>
         </Container>
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(FeedMain)
+export default withRouter(FeedMain);
