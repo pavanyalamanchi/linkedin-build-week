3bb21714893e1066da45e69ff46a84ecf47de86b
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-21 12:56:55 +0200

    Update FeedMain.jsx

--- src/components/feed/FeedMain.jsx
+++ src/components/feed/FeedMain.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 export default class FeedMain extends Component {
 	render() {
 		return (
 			<div>
-				FeedMain!
-			</div>
+          <Container>
+            <Row>
+              <Col sm={3}> <Sidebar /></Col>
+              <Col sm={5}></Col>
+              <Col sm={4}> <Sidebar /></Col>
+            </Row>
+          </Container>
+        </div>
 		)
 	}
 }
