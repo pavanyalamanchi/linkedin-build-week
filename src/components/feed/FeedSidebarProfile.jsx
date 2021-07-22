ae872fd4f23b1ccc284b253d5b9fef0d19a1670f
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-22 22:42:06 +0200

    FeedSideBar now has props data

--- src/components/feed/FeedSidebarProfile.jsx
+++ src/components/feed/FeedSidebarProfile.jsx
@@ -4,43 +4,16 @@ import { useState, useEffect } from "react";
 import cover from "../../assets/cover.jpg";
 
 export default function FeedSidebarProfile(props) {
-  const [profileData, setProfileData] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/profile/me",
-          {
-            headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGY1M2IyNTBlZmU3ODAwMTU1YzM0YTAiLCJpYXQiOjE2MjY2ODQxOTcsImV4cCI6MTYyNzg5Mzc5N30.3ZXfLM8Xio4MkKGlFiTA42FVjeiUinuO7VDCroKKFMw",
-            },
-          }
-        );
-        if (response.ok) {
-          let resp = await response.json();
-          setProfileData(resp);
-          console.log("fetch", resp);
-        } else {
-        }
-      } catch (error) {}
-    };
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-
 	return (
   	<Card className="mt-2">
             <div style={{backgroundImage: `url(${cover})`}} className="siderbarcover"></div>
             <div className="d-flex justify-content-center">
-            <img src={profileData.image} alt="#" className="rounded-circle align-self-center sidebarProfilePhoto"
+            <img src={props.user.image} alt="#" className="rounded-circle align-self-center sidebarProfilePhoto"
             />
             </div>
             <Card.Body className="leftSidebarCardBody">
-              <h3 style={{fontSize:"16px"}} className="text-center mt-3">{profileData.name} {profileData.surname}</h3>
-              <p className="text-center font-weight-light" >{profileData.title}</p>
+              <h3 style={{fontSize:"16px"}} className="text-center mt-3">{props.user.name} {props.user.surname}</h3>
+              <p className="text-center font-weight-light" >{props.user.title}</p>
               <hr></hr>
               <a href="#" className="text-muted" >
               <div className="d-flex justify-content-between">
@@ -62,8 +35,8 @@ export default function FeedSidebarProfile(props) {
               <path d="M20 20a3.36 3.36 0 001-2.39V6.38A3.38 3.38 0 0017.62 3H6.38A3.36 3.36 0 004 4z" fill="#f8c77e"></path>
               <path d="M4 4a3.36 3.36 0 00-1 2.38v11.24A3.38 3.38 0 006.38 21h11.24A3.36 3.36 0 0020 20z" fill="#e7a33e"></path>
               </svg></li-icon>
-              <span >
-              Try Premium for free
+              <span>
+                Try Premium for free
               </span>
               </span>
               </a>
