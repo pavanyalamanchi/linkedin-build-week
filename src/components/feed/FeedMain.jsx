23e98aeb3f89778c7f8ac168c877c64c990676f4
Marcelo Mantilla <marcelo@appcabinet.com>
2021-07-22 22:40:57 +0200

    Feed main, preparing fetch request with API key

--- src/components/feed/FeedMain.jsx
+++ src/components/feed/FeedMain.jsx
@@ -5,25 +5,37 @@ import FeedRightSidebar from './FeedRightSidebar'
 import { withRouter } from 'react-router-dom'
 import PostsContainer from "./PostsContainer";
 import NewPost from "./NewPost";
+import Loading from '../Loading'
 
 
 class FeedMain extends Component {
+  
+  componentDidMount = () => {
+    const fetchUser = fetch("... userAPI")
+  }
+  
   render() {
-    return (
-      <div>
-        {console.log("FeedMain - SIGN IN DATA:", this.props.user)}
-        <Container>
-          <Row>
-            <Col sm={3} className="rightSidebar"> <FeedLeftSidebar /></Col>
-            <Col sm={5} className="feedMain"> 
-              <NewPost user={this.props.user}/>
-              <PostsContainer />
+    if (this.props.user === null) {
+      return <Loading />
+    } else {
+      return (
+        <div>
+          {console.log("FeedMain - SIGN IN DATA:", this.props.user)}
+          <Container>
+            <Row>
+              <Col sm={3} className="rightSidebar"> 
+                <FeedLeftSidebar user={this.props.user}/>
               </Col>
-            <Col sm={4}> <FeedRightSidebar /></Col>
-          </Row>
-        </Container>
-      </div>
-    );
+              <Col sm={5} className="feedMain"> 
+                <NewPost user={this.props.user}/>
+                <PostsContainer user={this.props.user}/>
+                </Col>
+              <Col sm={4}> <FeedRightSidebar /></Col>
+            </Row>
+          </Container>
+        </div>
+      );
+    }
   }
 }
 
