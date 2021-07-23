ad86f8561b25f6e49ac1c42a274c08cad21e14e3
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-07-23 12:28:44 +0200

    sidebar sticky done

--- src/components/feed/SidebarAd.jsx
+++ src/components/feed/SidebarAd.jsx
@@ -2,11 +2,26 @@ import React from 'react'
 import {Card} from 'react-bootstrap'
 import marcelo from '../../assets/Marcelo-pic.png'
 import strive from '../../assets/strivelogo.jpg'
+import { useEffect, useState } from "react";
 
 export default function SidebarAd(props) {
+const [scrolled,setScrolled]=useState(false);
+    const handleScroll=() => {
+        const offset=window.scrollY;
+        if(offset > 200 ){
+          setScrolled(true);
+        }
+        else{
+          setScrolled(false);
+        }
+      }
+    
+      useEffect(() => {
+        window.addEventListener('scroll',handleScroll)
+      })
   
 	return (
-            <Card className="mt-2">
+            <Card className={`mt-2 ${scrolled ? 'sidebar-sticky' : ''} d-none d-lg-block`}>
             <Card.Body className="leftSidebarCardBody">
             <p className="text-center text-muted mt-2 mb-1">Get the latest jobs and industry news</p>
             <div className="d-flex justify-content-center">
